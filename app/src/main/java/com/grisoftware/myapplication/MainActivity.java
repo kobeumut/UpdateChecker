1fa2e2f9dd241528d9578def9993d49607273ed5
Umut ADALI <umut.adali@tsoft.club>
2017-10-09 13:51:25 +0300

    Fixing for library

---
+++ app/src/main/java/com/grisoftware/myapplication/MainActivity.java
@@ -0,0 +1,9 @@
+package com.grisoftware.myapplication;
+
+import android.app.Application;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+}
